f3a89801194111ecfca740e140078bb3417d65c5
Chris-White2024 <112902239+Chris-White2024@users.noreply.github.com>
2024-02-20 13:00:52 -0500

    Updated Debug Play Ground to Have Endgoal

---
+++ Assets/Scripts/endGoal.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class endGoal : MonoBehaviour
+{
+    //Declares all required UI components to put text on user's screen
+    public TMP_Text text;
+    bool win = false;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            text.text = "You Win!" + "\n" + "Time taken: " + Time.timeSinceLevelLoad.ToString() + " seconds";
+            win = true;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (win)
+        {
+        
+        }
+        else{
+        text.text = "Reach the top of the Castle to win!" + "\n" + "Time taken so far" + Time.timeSinceLevelLoad.ToString() + " seconds";
+        }    
+    }
+
+}
