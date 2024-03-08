f05c1b6baecf561c822dfba9ebe6a6d2626fbf2b
Chris-White2024 <cwhite15@terpmail.umd.edu>
2024-03-08 02:00:25 -0500

    changed many thing

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -7,12 +7,12 @@ using System;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
-    Vector3 velocity;
+    public Vector3 velocity;
     public Transform cam;
     public float speed = 6f;
     public float turnSmoothTime = .1f;
     float turnSmoothVelocity;
-    float gravity = -9.81f;
+    public float gravity = -9.81f;
     public Transform groundCheck;
     public float groundDistance;
     public LayerMask groundMask;
@@ -21,13 +21,14 @@ public class ThirdPersonMovement : MonoBehaviour
     public GameObject playerHead;
     public float blinkDistance = 5.0f;
     ColorIndicator colorOnScreen;
+    public bool hasDoubleJump;
 
     //Make a queue to store the colors so its first in first out
     List<string> colorStack = new List<string>();    
 
 
-    bool isGrounded;
-    bool hasUmbrella = false;
+    public bool isGrounded;
+
 
     void Start(){
         colorOnScreen = GameObject.Find("Canvas").GetComponent<ColorIndicator>();
@@ -40,38 +41,7 @@ public class ThirdPersonMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E)){
             popColor();
         }
-        if(Input.GetKeyDown(KeyCode.Q)){
-            popFromEnd();
-        }
-        if(velocity.y != -2 && velocity.y < 0 && hasUmbrella){
-            gravity = -2.5f;
-
-        }
-        else{
-            gravity = -9.81f;
-        }
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if(direction.magnitude >= .1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            controller.Move(moveDir * speed * Time.deltaTime);
-        }
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-
-        if(isGrounded && velocity.y < 0){
-            velocity.y = -2f;
-        }
-        if(Input.GetButtonDown("Jump") && isGrounded){
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-        }
+        movementFromInput();
     }
     public void pushColor(string color){
 
@@ -112,39 +82,35 @@ public class ThirdPersonMovement : MonoBehaviour
         speed = 6f;
         jumpHeight = 5.0f;
     }
-    void popFromEnd(){
-        resetAbilities();
-        if (colorStack.Count == 0){
-            playerBody.GetComponent<Renderer>().material.color = Color.white;
-            playerHead.GetComponent<Renderer>().material.color = Color.white;
-            return;
-        }
-        string color = colorStack[colorStack.Count - 1]; // Accessing last element using index notation
-        colorStack.RemoveAt(colorStack.Count - 1);
-        if (color == "blue"){
-            playerBody.GetComponent<Renderer>().material.color = Color.blue;
-            playerHead.GetComponent<Renderer>().material.color = Color.blue;
-        }
-        else if (color == "green"){
-            playerBody.GetComponent<Renderer>().material.color = Color.green;
-            playerHead.GetComponent<Renderer>().material.color = Color.green;
-            jumpHeight = 10.0f;
-        }
-        else if (color == "red"){
-            playerBody.GetComponent<Renderer>().material.color = Color.red;
-            playerHead.GetComponent<Renderer>().material.color = Color.red;
-            speed = 20f;
-        }
-        colorOnScreen.UpdateColorBlocks(colorStack);
-    }
-    public void pickUpGlider(){
-        hasUmbrella = true;
-    }
     public List<string> getColors(){
         return colorStack;
     }
 
     public float getMagnitude(){
         return Math.Abs(velocity.x + velocity.z);
+    }
+    void movementFromInput(){
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= .1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            controller.Move(moveDir * speed * Time.deltaTime);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+        if(Input.GetButtonDown("Jump") && (isGrounded || hasDoubleJump)){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
     }
 }
--- Assets/Scripts/pickupItem.cs
+++ Assets/Scripts/pickupItem.cs
@@ -5,23 +5,96 @@ using UnityEngine;
 public class pickupItem : MonoBehaviour
 {
     GameObject ompo;
-    GameObject gliderPos;
+    ThirdPersonMovement ompoMovement;
+    Vector3 offSet;
+    bool hasGlider = false;
+    bool hasBoots = false;
+    Vector3 originalPosition;
+    Quaternion originalRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         ompo = GameObject.Find("Third Person Player 1");
-        gliderPos = GameObject.Find("GliderPos");
+        ompoMovement = ompo.GetComponent<ThirdPersonMovement>();
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+        
     }
 
     void OnTriggerEnter(Collider other){
         this.transform.parent = ompo.transform;
+        if (this.name.Contains("Glider")){
+            hasGlider = true;
+            offSet = new Vector3 (0f,.35f,0f);
+            transform.localRotation = Quaternion.identity;
+        }
+        if (this.name.Contains("Boots")){
+            hasBoots = true;
+            ompoMovement.hasDoubleJump = true;
+            offSet = new Vector3 (.5f,-1f,.5f);
+            transform.localRotation = Quaternion.Euler(0f, 180.0f, 0.0f);
+        }
         // Match position and rotation with the collided object
         transform.localPosition = Vector3.zero;
-        transform.localPosition += new Vector3 (0f,.35f,0f);
-        transform.localRotation = Quaternion.identity;
-        ThirdPersonMovement moveScript = ompo.GetComponent<ThirdPersonMovement>();
-        moveScript.pickUpGlider();
+        transform.localPosition += offSet;
+    }
+    void Update(){
+        if (hasBoots && !(ompoMovement.hasDoubleJump)){
+            if(ompoMovement.isGrounded){
+                ompoMovement.hasDoubleJump = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Q)){
+            dropItem();
+            foreach (GameObject obj in getItemsToRemove()){
+                if (obj.transform.parent == ompo.transform){
+                    transform.parent = null;
+                    transform.position = originalPosition;
+                    transform.rotation = originalRotation;
+                }
+            }
+
+        }
+        if (Input.GetButtonDown("Jump")){
+            StartCoroutine(doubleJump());
+        }
+        gliderGravity();
+    }
+    void gliderGravity(){
+        if(ompoMovement.velocity.y != -2 && ompoMovement.velocity.y < 0 && hasGlider){
+            ompoMovement.gravity = -2.5f;
+        }
+        else{
+            ompoMovement.gravity = -9.81f;
+        }
+    }
+
+    IEnumerator doubleJump(){
+        yield return new WaitForSeconds(1);
+        ompoMovement.hasDoubleJump = false;    
+    }
+    void dropItem(){
+        hasGlider = false;
+        hasBoots = false;
+    }
+    List<GameObject> getItemsToRemove(){
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+
+        // List to store GameObjects with the script
+        List<GameObject> gameObjectsWithScript = new List<GameObject>();
+
+        // Loop through each GameObject
+        foreach (GameObject obj in allObjects)
+        {
+            // Check if the GameObject has the script attached
+            if (obj.GetComponent<pickupItem>() != null)
+            {
+                // Add the GameObject to the list
+                gameObjectsWithScript.Add(obj);
+            }
+        }
+        return gameObjectsWithScript;
     }
 
 }
