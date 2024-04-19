7a2d09be766cf00ab3d03046c8b8cd845340a708
Chris-White2024 <cwhite15@terpmail.umd.edu>
2024-04-19 09:29:41 -0400

    Updated menu's and added fan mechanics

---
+++ Assets/Scripts/FanBlow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FanBlow : MonoBehaviour
+{
+    public float blowForce = 10f;
+
+    void OnTriggerStay(Collider other)
+    {
+        // Check if the collider belongs to the player
+        if (other.CompareTag("Player"))
+        {
+            ThirdPersonMovement playerMovement = other.GetComponent<ThirdPersonMovement>();
+            if (playerMovement != null)
+            {
+                // Get the direction the fan is facing
+                Vector3 fanDirection = transform.forward.normalized;
+
+                // Calculate the force vector to be applied
+                Vector3 fanBlowForce = fanDirection * blowForce;
+
+                // Apply the force to the player's position
+                playerMovement.controller.Move(fanBlowForce * Time.deltaTime);
+            }
+        }
+    }
+}
