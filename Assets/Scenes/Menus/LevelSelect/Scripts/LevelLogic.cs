6a5f117c4ba065ec83202d2e1e2754453c1deb36
cmckee12 <cmckee12@terpmail.umd.edu>
2024-05-01 17:51:40 -0400

    stuff

--- Assets/Scenes/Menus/LevelSelect/Scripts/LevelLogic.cs
+++ Assets/Scenes/Menus/LevelSelect/Scripts/LevelLogic.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLogic : MonoBehaviour
 {
+
+    private int currentLevel;
+
+    public int level;
+    public Sprite locked;
+    public Sprite unlocked;
+
+    private Image image;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        image = GetComponent<Image>();
+        currentLevel = PlayerPrefs.GetInt("Level");
+
+        if (currentLevel >= level) {
+            image.sprite = unlocked;
+
+        } else {
+            image.sprite = locked;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void playLevel() {
+        SceneManager.LoadScene(level);
     }
+
 }
