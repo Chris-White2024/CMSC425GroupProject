9260df6355c9b6f248962d9629ac2c4e326e957c
Chris-White2024 <cwhite15@terpmail.umd.edu>
2024-05-04 09:14:07 -0400

    Added Level 3 and 4 as well as Audio

--- Assets/Scripts/vialCollectable.cs
+++ Assets/Scripts/vialCollectable.cs
@@ -6,9 +6,12 @@ public class vialCollectable : MonoBehaviour
 {
     private string sceneName;
     public bool debugMode = true;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
     void Start(){
         //Get Name of Scene We are in
         sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+        audioSource = GetComponent<AudioSource>();
         //Check if we have collected the vial in this scene
         if(PlayerPrefs.GetInt(sceneName + " vial") == 1 && !debugMode){
             Destroy(gameObject);
@@ -17,9 +20,17 @@ public class vialCollectable : MonoBehaviour
     }
     public void OnTriggerEnter(Collider other){
         if(other.gameObject.tag == "Player"){
+            //Play the audio clip
+            audioSource.clip = audioClip;
+            audioSource.Play();
             PlayerPrefs.SetInt(sceneName + " vial", 1);
             //Destroy the vial  
-            Destroy(gameObject);
+            if(audioSource.isPlaying){
+                Destroy(gameObject, audioClip.length);
+            }
+            else{
+                Destroy(gameObject);
+            }
         }
     }
 }
