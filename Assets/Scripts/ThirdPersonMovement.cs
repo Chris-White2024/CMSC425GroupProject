59f4c023697513f2fd2fd06c055be47d7622b95c
Chris-White2024 <cwhite15@terpmail.umd.edu>
2024-05-05 19:54:09 -0400

    Commented more code, made super jump and gliding easier

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -36,61 +37,72 @@ public class ThirdPersonMovement : MonoBehaviour
         MovementFromInput();
     }
 
-void MovementFromInput()
-{
-    isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-    isWallRunning = false;
-
-    // Check if the player is touching a wall tagged with "WallRun"
-    if (canWallRun && ((Physics.Raycast(transform.position, transform.right, out RaycastHit hitRight, 1.5f) && hitRight.collider.CompareTag("WallRun")) ||
-        (Physics.Raycast(transform.position, -transform.right, out RaycastHit hitLeft, 1.5f) && hitLeft.collider.CompareTag("WallRun"))))
+    //Function to move the player based on input
+    void MovementFromInput()
     {
-        // Change the gravity direction to simulate walking on the wall
-        isWallRunning = true;
-    }
+        //Check if the player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isWallRunning = false;
 
-    float horizontal = Input.GetAxisRaw("Horizontal");
-    float vertical = Input.GetAxisRaw("Vertical");
-    Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-    if (direction.magnitude >= .1f)
-    {
-        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-        // Apply wall running gravity if the player is wall running
-        float verticalVelocity = isWallRunning ? 0 : velocity.y;
-        controller.Move((moveDir * speed + Vector3.up * verticalVelocity) * Time.deltaTime);
-    }
-
-    // Apply regular gravity if not wall running
-    if (isWallRunning && velocity.y < 0 && !isGrounded){
-        velocity.y += (1/4) * gravity * Time.deltaTime;
-    }
-    else
-    {
-        velocity.y += gravity * Time.deltaTime;
-    }
-
-    controller.Move(velocity * Time.deltaTime);
-
-    if (isGrounded && velocity.y < 0)
-    {
-        velocity.y = -2f;
-    }
-
-    if (Input.GetButtonDown("Jump"))
-    {
-        if (isGrounded || isWallRunning)
+        // Check if the player is touching a wall tagged with "WallRun"
+        if (canWallRun && ((Physics.Raycast(transform.position, transform.right, out RaycastHit hitRight, 1.5f) && hitRight.collider.CompareTag("WallRun")) ||
+            (Physics.Raycast(transform.position, -transform.right, out RaycastHit hitLeft, 1.5f) && hitLeft.collider.CompareTag("WallRun"))))
         {
-            audioSource.clip = jumpSound;
-            audioSource.Play();
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            // Change the gravity direction to simulate walking on the wall
+            isWallRunning = true;
         }
+
+        //Get input from the player
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+        //Move the player based on input
+        if (direction.magnitude >= .1f)
+        {
+            //Source for the following 4 lines of code: https://www.youtube.com/watch?v=4HpC--2iowE
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            // Apply wall running gravity if the player is wall running
+
+            float verticalVelocity = isWallRunning ? 0 : velocity.y;
+            controller.Move((moveDir * speed + Vector3.up * verticalVelocity) * Time.deltaTime);
+        }
+
+        // Apply regular gravity if not wall running
+        if (isWallRunning && velocity.y < 0 && !isGrounded)
+        {
+            velocity.y += (1 / 4) * gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //Check if the player wants to jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded || isWallRunning)
+            {
+                audioSource.clip = jumpSound;
+                audioSource.Play();
+                velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            }
+        }
+
     }
-}
 
 
 }
--- Assets/Scripts/combat.cs
+++ Assets/Scripts/combat.cs
@@ -4,105 +4,119 @@ using UnityEngine;
 
 using UnityEngine.SceneManagement;
 
-
 public class combat : MonoBehaviour
 {
-    public int health = 5;
-
-    public ThirdPersonMovement playerMovement;
-    public float pushForce = 5f;
-
-    public GameObject ompoMesh;
+    public int health = 5; // Current health of the character
+    public ThirdPersonMovement playerMovement; // Reference to the ThirdPersonMovement component of the player
+    public float pushForce = 5f; // Force applied when pushed
+    public GameObject ompoMesh; // Reference to the mesh of the character
 
     void Start()
     {
-
+        // Finding and assigning the ThirdPersonMovement component of the player
         playerMovement = GameObject.Find("Third Person Player 1").GetComponent<ThirdPersonMovement>();
     }
 
+    // Method to decrease health when taking damage
     public void TakeDamage(int damage)
     {
-        health -= damage;
-        if (this.gameObject.tag == "Player"){
-            this.GetComponent<displayHealth>().updateHealth();
-        }
-    }
-    public void Heal(int heal)
-    {
-        health += heal;
+        health -= damage; // Decrease health by the specified amount
+        // If the character is the player, update the health display
         if (this.gameObject.tag == "Player"){
             this.GetComponent<displayHealth>().updateHealth();
         }
     }
 
+    // Method to increase health when healing
+    public void Heal(int heal)
+    {
+        health += heal; // Increase health by the specified amount
+        // If the character is the player, update the health display
+        if (this.gameObject.tag == "Player"){
+            this.GetComponent<displayHealth>().updateHealth();
+        }
+    }
+
+    // Called when the character collides with another object
     public void OnTriggerEnter(Collider other)
     {
+        // If the collision is with an enemy, push back and take damage
         if (other.gameObject.tag == "Enemy")
         {
             pushBack(other);
             TakeDamage(1);
-            StartCoroutine(flashMesh());
+            StartCoroutine(flashMesh()); // Flash the character's mesh to indicate damage
         }
+        // If the collision is with a hazard, push up and take damage
         else if (other.gameObject.tag == "Hazard")
         {
             pushUp();
             TakeDamage(2);
-            StartCoroutine(flashMesh());
+            StartCoroutine(flashMesh()); // Flash the character's mesh to indicate damage
         }
+        // If the collision is with another player and the player has boots, take damage and push up
         else if (other.gameObject.tag == "Player" && playerMovement.hasBoots == true)
         {
             TakeDamage(1);
             pushUpOmpo(other);
-            StartCoroutine(flashMesh());
+            StartCoroutine(flashMesh()); // Flash the character's mesh to indicate damage
         }
     }
 
+    // Method to push up the player
     public void pushUpOmpo(Collider other)
     {
         Vector3 upWards = other.transform.up.normalized;
-        //Reset ompoGravity to 0 to avoid player falling back down
+        // Reset the player's vertical velocity to prevent falling back down
         other.GetComponent<ThirdPersonMovement>().velocity.y = 0f;
         other.GetComponent<ThirdPersonMovement>().controller.Move(upWards * pushForce);
     }
+
+    // Method to push back the player or enemy
     public void pushBack(Collider other)
     {
         Vector3 pushDirection = other.transform.forward.normalized;
-        //Angle Slightly up to avoid pushing player into the ground
+        // Angle slightly upwards to avoid pushing the character into the ground
         pushDirection.y = 0.5f;
-        //Apply force in opposite direction for a short time
+        // Apply force in the opposite direction for a short time
         playerMovement.controller.Move(pushDirection * pushForce); 
     }
+
+    // Method to push up the player
     public void pushUp()
     {
         Vector3 upWards = this.transform.up.normalized;
-        //Reset ompoGravity to 0 to avoid player falling back down
+        // Apply upward force to the player
         playerMovement.controller.Move(upWards * pushForce); 
-        playerMovement.velocity.y = 0f;
+        playerMovement.velocity.y = 0f; // Reset vertical velocity to prevent falling back down
     }
+
     // Update is called once per frame
     void Update()
     {
+        // If health drops to or below zero
         if (health <= 0)
         {
+            // If the character is the player, load the death screen scene
             if (this.gameObject.tag == "Player"){
-            SceneManager.LoadScene("deathScreen");
+                SceneManager.LoadScene("deathScreen");
             }
             else{
-                //Destroy enemy and all parent objects
+                // Destroy the enemy and all parent objects
                 Destroy(this.gameObject.transform.parent.gameObject);
             }
         }
     }
 
+    // Coroutine to flash the character's mesh to indicate damage
     IEnumerator flashMesh(){
-
-        MeshRenderer mesh = ompoMesh.GetComponent<MeshRenderer>();
+        MeshRenderer mesh = ompoMesh.GetComponent<MeshRenderer>(); // Get the mesh renderer of the character
         for (int i = 0; i < 5; i++)
         {
-            mesh.enabled = false;
-            yield return new WaitForSeconds(0.1f);
-            mesh.enabled = true;
-            yield return new WaitForSeconds(0.1f);
+            mesh.enabled = false; // Disable the mesh renderer
+            yield return new WaitForSeconds(0.1f); // Wait for a short time
+            mesh.enabled = true; // Enable the mesh renderer
+            yield return new WaitForSeconds(0.1f); // Wait for a short time
         }
     }
 }
